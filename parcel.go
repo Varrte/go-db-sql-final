64a5d68f40f3232a355a88d4e7429f77aabc5118
Vartan Abramov <vartan.spb@gmail.com>
2024-04-08 16:35:27 +0300

    edit func

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -15,34 +16,58 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
 
+	res, err := s.db.Exec("INSERT INTO parsel(client,status,address,created_at) VALUES (:client,:status,:address,:created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	id, err := res.LastInsertId()
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	return int(id), err
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
 	// реализуйте чтение строки по заданному number
 	// здесь из таблицы должна вернуться только одна строка
+	row := s.db.QueryRow("SELECT client,status,address,created_at FROM parsel WHERE id=:number",
+		sql.Named("number", number))
+	//defer row.Close()
 
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
+	err := row.Scan(&p.Client, &p.Status, &p.Address, &p.CreatedAt)
 
-	return p, nil
+	return p, err
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 	// реализуйте чтение строк из таблицы parcel по заданному client
 	// здесь из таблицы может вернуться несколько строк
+	rows, err := s.db.Query("SELECT client,status,address,created_at FROM parsel WHERE id=:number",
+		sql.Named("number", client))
 
 	// заполните срез Parcel данными из таблицы
 	var res []Parcel
+	i := 0
+	for rows.Next() {
 
-	return res, nil
+		err := rows.Scan(res[i])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		i++
+	}
+	return res, err
 }
 
 func (s ParcelStore) SetStatus(number int, status string) error {
 	// реализуйте обновление статуса в таблице parcel
+	_, err := s.db.Exec("UPDATE parsel SET status =:status WHERE id=:id)",
+		sql.Named("status", status),
+		sql.Named("id", number))
 
-	return nil
+	return err
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
